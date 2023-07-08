58a444cd927f95e57f989d35b8f40ba7f77f09fb
sefeoluwa <sefeoluwa01@gmail.com>
2023-07-08 20:59:45 +0100

    imported button component. Works!

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import './App.css'
-
+import Button from './Button'
 function App() {
    const myName = "Sefeoluwa";
   return (
     <>
       <h1>Hello {myName}</h1>
-     
+     <Button />
      
     </>
   )
