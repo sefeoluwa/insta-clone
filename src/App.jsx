2dd97e15617789c778fced2f5e64c3258886a809
sefeoluwa <sefeoluwa01@gmail.com>
2023-07-08 20:52:39 +0100

    deleted unneccessary code

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,12 @@
-import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+   const myName = "Sefeoluwa";
   return (
     <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is now {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Hello {myName}</h1>
+     
+     
     </>
   )
 }
