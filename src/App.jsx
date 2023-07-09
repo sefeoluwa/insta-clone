2deb39b5c8c6873579ae5dcc269b10559a494561
sefeoluwa <sefeoluwa01@gmail.com>
2023-07-09 08:54:19 +0100

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import './App.css'
-import Button from './Button'
+import Nav from './components/Nav'
+
 function App() {
-   const myName = "Sefeoluwa";
-   const myAge = 22
+  
   return (
-    <>
-      <h1>Hello {myName} and my age is {myAge}</h1>
-     <Button alertText="Hello Everyone"/>
-     
-    </>
+   <div className="App">
+    <Nav />
+   </div>
   )
 }
 
