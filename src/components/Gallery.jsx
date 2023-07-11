f49338e7ee7238b19cbbb8bc6bd9b4ca0d82fa02
sefeoluwa <sefeoluwa01@gmail.com>
2023-07-11 19:27:08 +0100

    gallery images removed with state

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,10 +1,8 @@
-import pexels1 from '../assets/pexels1.jpeg'
-import pexels2 from '../assets/pexels2.jpeg'
-import pexels3 from '../assets/pexels3.jpeg'
-import pexels4 from '../assets/pexels4.jpeg' 
-import pexels5 from '../assets/pexels5.jpeg'  
+import { useState } from 'react'
 
 const Gallery = () => {
+     const [allPhotos, setAllPhotos] = useState([])
+
     return (
         <>
         <input type="file" name='photo' id='addPhotoInput' />
@@ -13,31 +11,13 @@ const Gallery = () => {
         </label>
 
         <section className="gallery">
-            <div className="item">
-                 <img src={pexels1} alt="gallery " className="item-image" />
+        {allPhotos.map((photo) => {
+          <div className="item" key={photo.id}>
+                 <img src={photo.url} alt="gallery "  className="item-image" />
                  <button className="delete-button">Delete</button>
             </div>
-
-            <div className="item">
-                 <img src={pexels2} alt="gallery " className="item-image" />
-                 <button className="delete-button">Delete</button>
-            </div>
-
-            <div className="item">
-                 <img src={pexels3} alt="gallery " className="item-image" />
-                 <button className="delete-button">Delete</button>
-            </div>
-
-            <div className="item">
-                 <img src={pexels4} alt="gallery " className="item-image" />
-                 <button className="delete-button">Delete</button>
-            </div>
-
-            <div className="item">
-                 <img src={pexels5} alt="gallery " className="item-image" />
-                 <button className="delete-button">Delete</button>
-            </div>
-        </section>
+        })}
+      </section>
         </>
     )
 }
