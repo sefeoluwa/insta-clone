bbccf049d8797026afae5989d21c4bf09c6ac278
sefeoluwa <sefeoluwa01@gmail.com>
2023-07-11 13:31:39 +0100

    nav bar icons

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -6,6 +6,21 @@ const Nav = () => {
             <button className="logo">
                 <img src={instagramLogo} alt="" />
             </button>
+            <input type="text" className="search" placeholder='search'></input>
+            <span className='nav-links'>
+                <button>
+                    <i className="fas fa-home"></i>
+                </button>
+                <button>
+                    <i className="fas fa-comment-alt"></i>
+                </button>
+                <button>
+                    <i className="fas fa-compass"></i>
+                </button>
+                <button>
+                    <i className="fas fa-heart"></i>
+                </button>
+            </span>
         </nav>
     )
 }
