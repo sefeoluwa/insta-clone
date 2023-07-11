352d98c1ff51486968a13c8fda7e88c87783ffca
sefeoluwa <sefeoluwa01@gmail.com>
2023-07-11 13:30:16 +0100

    Rendered Bio and Gallery

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import './App.css'
 import Nav from './components/Nav'
+import Bio from './components/Bio'
+import Gallery from './components/Gallery'
 
 function App() {
   
   return (
-   <div className="App">
+    <>
     <Nav />
+   <div className="container">
+    <Bio />
+    <Gallery />
    </div>
+   </>
   )
 }
 
