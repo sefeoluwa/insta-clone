0f5ea2c79db23a3f49b05b5cf2f0a2cb7656184e
sefeoluwa <sefeoluwa01@gmail.com>
2023-07-12 19:17:14 +0100

    new images adds to the top of the gallery

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -4,38 +4,38 @@ import { db } from '../dexie'
 import Loader from './Loader.jsx'
 
 const Gallery = () => {
-     const allPhotos = useLiveQuery(() => db.gallery.toArray(), [])
+  const allPhotos = useLiveQuery(() => db.gallery.toArray(), []);
 
-     const addPhoto = async () => {
-        db.gallery.add({
-          url: await getPhotoUrl('#addPhotoInput')
-        })
-     }
+  const addPhoto = async () => {
+    db.gallery.add({
+      url: await getPhotoUrl("#addPhotoInput")
+    });
+  };
 
-     const removePhoto = (id) => {
-      db.gallery.delete(id)
-     }
+  const removePhoto = (id) => {
+    db.gallery.delete(id);
+  };
 
+  const reversedPhotos = allPhotos ? [...allPhotos].reverse() : [];
 
-    return (
-        <>
-        <input type="file" name='photo' id='addPhotoInput' />
-        <label htmlFor="addPhotoInput" onClick= {addPhoto} >
-            <i className='add-photo-button fas fa-plus-square'></i>
-        </label>
+  return (
+    <>
+      <input type="file" name="photo" id="addPhotoInput" />
+      <label htmlFor="addPhotoInput" onClick={addPhoto}>
+        <i className="add-photo-button fas fa-plus-square"></i>
+      </label>
 
-        <section className="gallery">
+      <section className="gallery">
         {!allPhotos && <Loader />}
-  {allPhotos?.map((photo) => (
-    <div className="item" key={photo.id}>
-      <img src={photo.url} alt="gallery" className="item-image" />
-      <button className="delete-button" onClick={ () => removePhoto(photo.id)}>Delete</button>
-    </div>
-  ))}
-</section>
-
-        </>
-    )
-}
+        {reversedPhotos.map((photo) => (
+          <div className="item" key={photo.id}>
+            <img src={photo.url} alt="gallery" className="item-image" />
+            <button className="delete-button" onClick={() => removePhoto(photo.id)}>Delete</button>
+          </div>
+        ))}
+      </section>
+    </>
+  );
+};
 
 export default Gallery
