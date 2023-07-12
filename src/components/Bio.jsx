9561f9f426e8872a088ebfb93b9d70931bfac39e
sefeoluwa <sefeoluwa01@gmail.com>
2023-07-12 13:08:36 +0100

    bio now saves to database

--- src/components/Bio.jsx
+++ src/components/Bio.jsx
@@ -1,27 +1,42 @@
   import profileIcon from '../assets/profileIcon.svg'
-  import { useState } from 'react'
+  import { useState, useEffect } from 'react'
   import getPhotoUrl from 'get-photo-url'
+  import { db } from '../dexie'
   
   const Bio = () => {
     // Rendering edit form with the edit button conditionally
     const editButton = <button className="save-button" onClick={() => setEditFormIsOpen(true)}>Edit</button>
      const [editFormIsOpen, setEditFormIsOpen] = useState(false)
 
+
     //  Functions to get the details of the user and render it to the bio
      const [userDetails, setUserDetails] = useState({
         name: 'Sefeoluwa Akinbeye',
         about: 'Self-taught Frontend Developer.'
     })
-    const updateUserDetails = (e) => {
+    const updateUserDetails = async (e) => {
         e.preventDefault()
-        setUserDetails({
+        const objectData = {
             name: e.target.nameOfUser.value,
             about: e.target.aboutUser.value,
-            
-        })
-        setEditFormIsOpen(false)
+        }
+        setUserDetails(objectData)
 
+        // Update bio content in storage
+      await  db.bio.put(objectData, 'info')
+
+        setEditFormIsOpen(false)
     }
+    // GEt the bio data from index and save it to user details
+    useEffect(() => {
+        const setDataFromDb = async () => {
+            const userDetailsFromDb = await db.bio.get('info');
+          userDetailsFromDb &&  setUserDetails(userDetailsFromDb);
+          const profilePhotoFromDb = await db.bio.get('profilePhoto')
+          profilePhotoFromDb && setProfilePhoto(profilePhotoFromDb)
+        }
+        setDataFromDb()
+    }, [])
 
     // changing profile photo
     const [profilePhoto, setProfilePhoto] = useState(profileIcon)
@@ -29,13 +44,14 @@
     const updateProfilePhoto = async () => {
         const newProfilePhoto = await getPhotoUrl('#profilePhotoInput')
         setProfilePhoto(newProfilePhoto)
+        await db.bio.put(newProfilePhoto, 'profilePhoto')
     }
     
     // Bio form 
     const editForm =(
         <form className='edit-bio-form' action="" onSubmit= {(e) => updateUserDetails(e)}>
-             <input type="text" id='' name= 'nameOfUser' placeholder='Your Name'  />
-             <input type="text" id='' name='aboutUser' placeholder='About You'  />
+             <input type="text" id='' name= 'nameOfUser' placeholder='Your Name' defaultValue={userDetails?.name}  />
+             <input type="text" id='' name='aboutUser' placeholder='About You' defaultValue={userDetails?.about}  />
             <br />
             <button type='button' className='cancel-button' onClick= {() => setEditFormIsOpen(false)}>Cancel</button>
             <button  type='submit' >Save</button>
