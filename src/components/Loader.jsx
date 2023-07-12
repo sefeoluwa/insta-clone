93832c94e912705ba0920a74a77db9eb164c3d6a
sefeoluwa <sefeoluwa01@gmail.com>
2023-07-12 18:28:07 +0100

    initialized loader container

---
+++ src/components/Loader.jsx
@@ -0,0 +1,13 @@
+import '../loader.css';
+
+const Loader = () => {
+  return (
+    <div className="loadingio-spinner-ball-hzjsu3tyykt">
+      <div className="ldio-ldo8enty6y">
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default Loader;
